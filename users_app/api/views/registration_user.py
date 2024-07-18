0f05e6814165858993202700a82cd44a722b5ea1
Alejandro Zacarias <alez42676@gmail.com>
2024-07-18 02:24:55 -0600

    views de usuarios creado

---
+++ users_app/api/views/registration_user.py
@@ -0,0 +1,29 @@
+from users_app.api.serializers.user_serializer import RegisterUserSerializer
+from users_app.api.models import user_model
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterUser(APIView):
+    def post(self, request):
+        print(request)
+        serializer=RegisterUserSerializer(data=request.data)
+        data={}
+        if serializer.is_valid():
+            account=serializer.save()
+            
+            data['response']="Successfully registered"
+            data['username']=account.username
+            data['email']=account.email
+            
+            refresh = RefreshToken.for_user(account)
+            
+
+            data['token']= {
+                   'refresh': str(refresh),
+                   'access': str(refresh.access_token),
+                   }
+            return Response(data)
+        return Response(serializer.errors)
