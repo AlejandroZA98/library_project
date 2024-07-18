b10ee74d75eddbc4d456c413e98c634075b5dc92
Alejandro Zacarias <alez42676@gmail.com>
2024-07-18 02:28:05 -0600

    actualizacion de migraciones

--- library_app/api/models/review_model.py
+++ library_app/api/models/review_model.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from library_app.api.models.book_model import Book
-
+from django.contrib.auth.models import User
 class Review(models.Model):
+    review_user = models.ForeignKey(User,on_delete=models.CASCADE)
     books=models.ForeignKey(Book,on_delete=models.CASCADE,related_name="reviews")
     rating=models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     description=models.TextField()
